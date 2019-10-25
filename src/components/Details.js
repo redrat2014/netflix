7bb7af396874120513846413bafe06c0442b08a0
red rat <renatomar76@gmail.com>
2019-10-25 17:53:13 -0300

    Animatable Header + Native Elements

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import { 
-    View, 
-    Text, 
-    StyleSheet, 
-    Image,  
-    TouchableWithoutFeedback, 
-    ScrollView, 
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableWithoutFeedback,
+    ScrollView,
     Dimensions,
     Share
 } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import Orientation from 'react-native-orientation';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as Animatable from 'react-native-animatable';
+
 import TabsEpisodes from './TabsEpisodes';
-import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const { width, height } = Dimensions.get('window');
 
 export default class Details extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            measures: 0,
+            header: false,
+            animation: ''
+        }
+    }
 
     componentWillMount() {
         Orientation.lockToPortrait();
@@ -36,18 +46,31 @@ export default class Details extends Component {
     }
 
     onShare() {
-      Share.share({
-          title: 'Designated Survivor',
-          url: 'www.youtube.com',
-          message: 'Awesome Tv show'
-      }, {
-        //android
-        dialogTitle: 'Share this awesome content',
-        //ios
-        excludedActivityTypes: [
-            'com.apple.UIKit.activity.PostToTwitter'
-        ]
-      });
+        Share.share({
+            title: 'Designated Survivor',
+            url: 'www.youtube.com',
+            message: 'Awesome Tv show'
+        }, {
+            //android
+            dialogTitle: 'Share this awesome content',
+            //ios
+            excludedActivityTypes: [
+                'com.apple.UIKit.activity.PostToTwitter'
+            ]
+        });
+    }
+
+    handleScroll(event) {
+        if (event.nativeEvent.contentOffset.y > this.state.measures) {
+            this.setState({
+                header: true,
+                animation: 'slideInDown'
+            });
+        } else {
+            this.setState({
+                header: false
+            });
+        }
     }
 
     render() {
@@ -56,71 +79,90 @@ export default class Details extends Component {
         const { thumbnail, cast, description, year, creator, numOfEpisodes, season } = this.props.item.details;
 
         return (
-            <ScrollView style={styles.wrapper}>
-                <Image
-                    source={{ uri: thumbnail }}
-                    style={styles.thumbnail}
-                />
-                <View style={styles.buttonPlay}>
-                    <TouchableWithoutFeedback onPress={() => this.openVideo()}>
-                        <View>
-                            <Icon
-                                name="play-circle"
-                                color="white"
-                                size={90}
-                                style={styles.iconPlay}
-                            />
-                        </View>
-                    </TouchableWithoutFeedback>
-                </View>
-                <View style={styles.nameContainer}>
-                    <LinearGradient start={{x: 0, y: 0}} end={{x: 0, y: 1}} colors={['transparent', '#181818', '#181818']} style={styles.linearGradient}>
-                        <Text style={[styles.text, styles.titleShow]}>{name}</Text>
-                    </LinearGradient>
-                </View>
-                <View style={styles.descriptionWrapper}>
-                    <View style={styles.subtitle}>
-                        <Text style={[styles.text, styles.subtitleText]}>{year}</Text>
-                        <Text style={[styles.text, styles.subtitleText]}>{numOfEpisodes}</Text>
-                        <Text style={[styles.text, styles.subtitleText]}>{season} Season</Text>
-                    </View>
-                </View>
-                
-                <View style={styles.detailsWrapper}>
-                    <View style={styles.description}>
-                        <Text style={[styles.text, styles.lightText]}>{description}</Text>
-                    </View>
-                    <Text style={styles.text}>Cast: {cast}</Text>
-                    <Text style={styles.text}>Creator: {creator}</Text>
-                    <View style={styles.shareListIcons}>
-                        <View style={styles.myListIcon}>
-                            <Ionicons
-                                name="md-checkmark"
-                                color="grey"
-                                size={27}
-                                style={styles.listIcon}
-                            />
-                            <Text style={styles.text}>My List</Text>
-                        </View>
-                        <TouchableHighlight
-                            onPress={this.onShare}
-                        >
-                            <View style={styles.myShareIcon}>
+            <View style={styles.headerdWrapper}>
+               {this.state.header ? 
+                    <Animatable.View 
+                        animation={this.state.animation}
+                        style={styles.header}
+                    >
+                        <Text style={styles.headerText}>{name}</Text>
+                    </Animatable.View> 
+                : null}
+                <ScrollView
+                    onScroll={this.handleScroll.bind(this)}
+                    style={styles.wrapper}
+                >
+                    <Image
+                        source={{ uri: thumbnail }}
+                        style={styles.thumbnail}
+                    />
+                    <View style={styles.buttonPlay}>
+                        <TouchableWithoutFeedback onPress={() => this.openVideo()}>
+                            <View>
                                 <Icon
-                                    name="share-alt"
-                                    color="grey"
-                                    size={25}
-                                    style={styles.shareIcon}
+                                    name="play-circle"
+                                    color="white"
+                                    size={90}
+                                    style={styles.iconPlay}
                                 />
-                                <Text style={styles.text}>Share</Text>
                             </View>
-                        </TouchableHighlight>
-                        
+                        </TouchableWithoutFeedback>
                     </View>
-                </View>
+                    <View style={styles.nameContainer}
+                        onLayout={({ nativeEvent }) => {
+                            this.setState({
+                                measures: nativeEvent.layout.y
+                            });
+                        }}
+                    >
+                        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} colors={['transparent', '#181818', '#181818']} style={styles.linearGradient}>
+                            <Text style={[styles.text, styles.titleShow]}>{name}</Text>
+                        </LinearGradient>
+                    </View>
+                    <View style={styles.descriptionWrapper}>
+                        <View style={styles.subtitle}>
+                            <Text style={[styles.text, styles.subtitleText]}>{year}</Text>
+                            <Text style={[styles.text, styles.subtitleText]}>{numOfEpisodes}</Text>
+                            <Text style={[styles.text, styles.subtitleText]}>{season} Season</Text>
+                        </View>
+                    </View>
 
-                <TabsEpisodes data={episodes} />
-            </ScrollView>
+                    <View style={styles.detailsWrapper}>
+                        <View style={styles.description}>
+                            <Text style={[styles.text, styles.lightText]}>{description}</Text>
+                        </View>
+                        <Text style={styles.text}>Cast: {cast}</Text>
+                        <Text style={styles.text}>Creator: {creator}</Text>
+                        <View style={styles.shareListIcons}>
+                            <View style={styles.myListIcon}>
+                                <Ionicons
+                                    name="md-checkmark"
+                                    color="grey"
+                                    size={27}
+                                    style={styles.listIcon}
+                                />
+                                <Text style={styles.text}>My List</Text>
+                            </View>
+                            <TouchableHighlight
+                                onPress={this.onShare}
+                            >
+                                <View style={styles.myShareIcon}>
+                                    <Icon
+                                        name="share-alt"
+                                        color="grey"
+                                        size={25}
+                                        style={styles.shareIcon}
+                                    />
+                                    <Text style={styles.text}>Share</Text>
+                                </View>
+                            </TouchableHighlight>
+
+                        </View>
+                    </View>
+
+                    <TabsEpisodes data={episodes} />
+                </ScrollView>
+            </View>
         );
     }
 }
@@ -146,7 +188,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent',
     },
     descriptionWrapper: {
-        paddingHorizontal: 20,
+        paddingHorizontal: 25,
     },
     subtitle: {
         flexDirection: 'row',
@@ -187,10 +229,10 @@ const styles = StyleSheet.create({
         fontWeight: '100',
     },
     detailsWrapper: {
-        paddingHorizontal: 20
+        paddingHorizontal: 25
     },
     nameContainer: {
-        backgroundColor: 'transparent', 
+        backgroundColor: 'transparent',
     },
     titleShow: {
         fontSize: 35,
@@ -207,4 +249,21 @@ const styles = StyleSheet.create({
         width: '100%',
         marginTop: -35,
     },
+    headerdWrapper: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#181818',
+        paddingVertical: 10,
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1,
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 20
+    }
 });
